c223c0fbabff000a47d87d334288a9ef45c21950
Irina144 <solnechnayamichelle@gmail.com>
2025-07-22 22:17:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+
+        int miles = ticketPrice / 20;
+
+        System.out.println("Начисленные мили: " + miles);
+    }
+}
